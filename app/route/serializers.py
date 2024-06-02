5c78b5731b983f6336ea8126878a413320fd13d9
Rustam Abdulkadirov <hrust@192.168.1.6>
2024-06-02 06:25:31 +0300

    Исправление ошибок импорта данных остановок.

--- app/route/serializers.py
+++ app/route/serializers.py
@@ -22,25 +22,27 @@ class BusStopSerializer(serializers.ModelSerializer):
             # parse object type
             object_type_data = location_data.pop("object_type")
             if object_type_data:
-                object_type = ObjectType.objects.get_or_create(**object_type_data)
-                location_data["object_type"] = object_type[0]
+                object_type, created = ObjectType.objects.get_or_create(**object_type_data)
+                location_data["object_type"] = object_type
+            # parse point
             point_data = location_data.pop("point")
             if point_data:
-                point = ObjectPoint.objects.get_or_create(**point_data)[0]
-            location = MapObject.objects.get_or_create(**location_data)[0]
-            try:
-                location.point.delete()
+                point, created = ObjectPoint.objects.get_or_create(**point_data)
+            if created:
+                location = MapObject.objects.create(**location_data)
                 point.map_object = location
-            except MapObject.point.RelatedObjectDoesNotExist:
-                point.map_object = location
-            point.save()
+                point.save()
+            else:
+                location = point.map_object
             location_data["point"] = point
             validated_data["location"] = location
+            name_data = validated_data.pop("name")
             busstop, created = BusStop.objects.update_or_create(**validated_data)
             location.name = "busstop-" + str(busstop.id)
             location.save()
+            busstop.name = name_data
+            busstop.save()
             return busstop
-        # return BusStop.objects.create(**validated_data)
 
 
 class BusStopSimpleSerializer(serializers.ModelSerializer):
@@ -48,15 +50,13 @@ class BusStopSimpleSerializer(serializers.ModelSerializer):
         model = BusStop
         fields = ["id", "name"]
 
-    id = serializers.IntegerField(read_only=True)
-
 
 class RouteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Route
+        fields = ["id", "name", "path_a", "path_b", "path_a_stops", "path_b_stops"]
+
     path_a = MapObjectSerializer(many=False)
     path_b = MapObjectSerializer(many=False)
     path_a_stops = BusStopSerializer(many=True, required=False)
     path_b_stops = BusStopSerializer(many=True, required=False)
-
-    class Meta:
-        model = Route
-        fields = ["id", "name", "path_a", "path_b", "path_a_stops", "path_b_stops"]
